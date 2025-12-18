4e69d4beb1b9e4bc1fb94d5aac168ecee813a3fc
Safae EL azouzi <safaeelazouzi2007@gmail.com>
2025-12-18 09:42:31 +0100

    main.c
    Refactored the main function to include user input for calculating GCD using the Euclidean algorithm.

--- main.c
+++ main.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
+#include <stdio.h>
 
-// -------------------------------
-// Fonction pour calculer le PGCD
-// -------------------------------
-int pgcd(int a, int b) {
-    int reste;
-    while (b != 0) {
-        reste = a % b;
-        printf("%d = %d × (%d) + %d\n", a, b, a / b, reste);
-        a = b;
-        b = reste;
-    }
-    return a;
+int main()
+
+{
+    int a,b,r;
+   printf("Donner le premier nombre : ");
+   scanf("%d", &a);
+   printf("Donner le deuxieme nombre: ");
+   scanf("%d", &b);
+
+// Algorithme d'Euclide
+
+while (b != 0) {
+
+r = a % b;
+printf("a=%d,b=%d,r=a%%b=%d\n",a,b,r);
+
+a = b;
+
+b = r;
 }
 
-// ------------------------------------------------------
-// Algorithme d’Euclide étendu : calcule d, x, y tels que
-// a*x + b*y = d  (où d = pgcd(a,b))
-// ------------------------------------------------------
-int euclide_etendu(int a, int b, int *x, int *y) {
-    if (b == 0) {
-        *x = 1;
-        *y = 0;
-        return a;
-    }
+// Affichage du résultat
 
-    int x1, y1;
-    int d = euclide_etendu(b, a % b, &x1, &y1);
+printf("Le PGCD est: %d\n", a);
+if(a==1){
+printf("les deux nombres sont premiers entre eux");
+}
+    return 0;
+}
 
-    *x = y1;
-    *y = x1 - (a / b) * y1;
-
-    return d;
 
 
